20b2886a7a50732674efa7aa0c66dac6fd31f587
Jorge Adalberto Rodriguez Garcia <george_nlc@hotmail.com>
2022-01-10 23:25:50 -0600

    Questions and options added

--- js/options.js
+++ js/options.js
@@ -9,7 +9,15 @@ class Option {
 let optionsQuestion1 = new Option('A programming language', 'A software', 'A web page');
 let optionsQuestion2 = new Option('$', 'var', 'int');
 let optionsQuestion3 = new Option('Something', 'nothing', 'A block of code');
+let optionsQuestion4 = new Option('console.log()', 'printf()', 'System.out.println()');
+let optionsQuestion5 = new Option('function name(){}', 'const name = () => {}', 'let name = function(){}');
+let optionsQuestion6 = new Option('let myArray = [];', 'arry = []', 'int myArray[];');
+let optionsQuestion7 = new Option('New York', 'Beijing', 'Mexico');
+let optionsQuestion8 = new Option('To have our code organized', 'It does not have relevant', 'To model real world things');
+let optionsQuestion9 = new Option('let object = (key: value)', 'let object = {key: value}', 'let object = [key: value]');
+let optionsQuestion10 = new Option('_variableName;', 'private int variableName;', 'private: int variableName;');
 
-let optionsQuestions = [optionsQuestion1, 
-                        optionsQuestion2, 
-                        optionsQuestion3];
+let optionsQuestions = [optionsQuestion1, optionsQuestion2, optionsQuestion3,
+                        optionsQuestion4, optionsQuestion5, optionsQuestion6,
+                        optionsQuestion7, optionsQuestion8, optionsQuestion9,
+                        optionsQuestion10];
--- js/questions.js
+++ js/questions.js
@@ -8,5 +8,14 @@ class Question {
 let question1 = new Question('What is javaScript?', 'a');
 let question2 = new Question('How to declare a variable?', 'b');
 let question3 = new Question('What is a function', 'c');
+let question4 = new Question('Which method allow you to print in console?', 'a');
+let question5 = new Question('How to declare an arrow function?', 'b');
+let question6 = new Question('How to declare an array?', 'a');
+let question7 = new Question("const cities = ['New York', 'Beijing', 'Mexico']\nWhat is the output of cities[2]?", 'c');
+let question8 = new Question('What is the usage of objects in javaScript?', 'c');
+let question9 = new Question('What is the syntax of an object literal?', 'b');
+let question10 = new Question('How to declare a private variable in JavaScript?', 'a');
 
-let questions = [question1, question2, question3];
+let questions = [question1, question2, question3, question4,
+                 question5, question6, question7, question8,
+                 question9, question10];
